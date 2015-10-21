15ce8aeea60cd6b841a169767e71c0efa47bcee4
theotherdy <allotmentor@allotmentor.com>
2015-10-21 20:25:04 +0100

    Glow on change

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,25 @@
 (function() {
 	//var app = angular.module('pattern', ['pattern.learning', 'pattern.recall']);
 	var app = angular.module('pattern', ['frapontillo.bootstrap-switch','ui.grid', 'ui.grid.expandable']);
-
-})();
+	app.directive('highlightOnChange', ['$timeout', function($timeout) {
+		return {
+		    restrict: 'A',
+		    scope: {
+		      	model: '=highlightOnChange'
+		    },
+		    link: function(scope, element) {
+		      	scope.$watch('model', function (nv, ov) {
+		        	if (nv !== ov) {
+		          	// apply class
+		          	element.addClass('highlight');
+		          	// auto remove after some delay
+		          	$timeout(function () {
+		            	element.removeClass('highlight');
+		          		}, 750);
+		        	}
+		      	});
+		    	}
+		  	};
+		}]);
+	})();
 
